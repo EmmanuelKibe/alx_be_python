342e9f468ad27945b36b45bc44a1a51683a8a5d9
Emmanuel <emmanuelkibe148@gmail.com>
2025-06-08 22:11:37 +0400

    Created a temparature unit converter

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,21 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    
+temp = float(input("Input the temparature: "))
+confirmation = input("Is this temparature in Celcius(C) or Farenheit(F): ")
+
+if confirmation.strip() == 'C':
+    converted_temp = convert_to_fahrenheit(temp)
+    print(f"{temp}°C is {converted_temp}°F")
+
+elif confirmation.strip() == 'F':
+    converted_temp = convert_to_celsius(temp)
+    print(f"{temp}°F is {converted_temp}°C") 
+else:
+    print("Error! Input the correct units")
