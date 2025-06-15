d05530246ed2e8700a569024bd55b0dc6d9ffb1c
Emmanuel <emmanuelkibe148@gmail.com>
2025-06-15 10:59:51 +0400

    Bank account simulator with OOP

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,6 +15,6 @@ class BankAccount:
         
 
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance:.2f}") 
+        print(f"Current Balance: {self.account_balance:.2f}") 
     
 
